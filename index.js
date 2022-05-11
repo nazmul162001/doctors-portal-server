2be9bd69211eb15e4d195e99117432b08b4f13fd
nazmul162001 <nazmulprime162001@gmail.com>
2022-05-11 12:45:45 +0600

    creat get api & load data from database

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express');
-const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors');
 require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
 const app = express();
 const port = process.env.PORT || 5000
 
@@ -11,7 +12,30 @@ app.use(express.json());
 
 
 // URI
-const uri = "mongodb+srv://doctor_admin:<password>@cluster0.urspw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.urspw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+// client connection
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+// async await function
+async function run(){
+  try{
+    await client.connect();
+    const serviceCollection = client.db('doctors_portal').collection('services');
+
+    // api
+    app.get('/service', async(req, res)=> {
+      const query = {};
+      const cursor = serviceCollection.find(query);
+      const services = await cursor.toArray();
+      res.send(services);
+    })
+  }
+  finally{
+
+  }
+}
+
+// call run function
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
   res.send('Hello Fromo dostors portal')
