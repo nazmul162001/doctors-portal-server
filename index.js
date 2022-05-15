f8068c799ed5a1456c0b911c50eefa31d307cbca
nazmul162001 <nazmulprime162001@gmail.com>
2022-05-15 10:23:58 +0600

    create put api for update user info

--- index.js
+++ index.js
@@ -21,6 +21,7 @@ async function run(){
     await client.connect();
     const serviceCollection = client.db('doctors_portal').collection('services');
     const bookingCollection = client.db('doctors_portal').collection('bookings');
+    const userCollection = client.db('doctors_portal').collection('users');
 
     // api for get data from database
     app.get('/service', async(req, res)=> {
@@ -28,6 +29,19 @@ async function run(){
       const cursor = serviceCollection.find(query);
       const services = await cursor.toArray();
       res.send(services);
+    })
+
+    // put api
+    app.put('/user/:email', async(req, res)=> {
+      const email = req.params.email;
+      const user = req.body;
+      const filter = {email: email}
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: user,
+      };
+    const result = await userCollection.updateOne(filter, updateDoc, options);
+    res.send(result);
     })
 
     app.get('/available', async(req,res)=> {
