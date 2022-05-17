ebf35af409bd03e7431c9ff83ed1cb9e32aeb924
nazmul162001 <nazmulprime162001@gmail.com>
2022-05-17 16:19:34 +0600

    added delete api & sent data

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const jwt = require('jsonwebtoken');
 const cors = require('cors');
 require('dotenv').config();
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const verify = require('jsonwebtoken/verify');
 
 const app = express();
 const port = process.env.PORT || 5000
@@ -45,6 +46,19 @@ async function run(){
     const userCollection = client.db('doctors_portal').collection('users');
     const doctorCollection = client.db('doctors_portal').collection('doctors');
 
+
+    // verify admin for added doctor
+    const verifyAdmin = async(req,res, next) => {
+      const requester = req.decoded.email;
+      const requesterAccount = await userCollection.findOne({email: requester});
+      if(requesterAccount.role === 'admin'){
+        next();
+      }
+      else{
+        res.status(403).send({message: 'forbidden'});
+      }
+    }
+
     // api for get data from database
     app.get('/service', async(req, res)=> {
       const query = {};
@@ -83,21 +97,14 @@ async function run(){
     })
 
     // put api for admin users
-    app.put('/user/admin/:email', verifyJWT,  async(req, res)=> {
+    app.put('/user/admin/:email', verifyJWT, verifyAdmin,   async(req, res)=> {
       const email = req.params.email;
-      const requester = req.decoded.email;
-      const requesterAccount = await userCollection.findOne({email: requester});
-      if(requesterAccount.role === 'admin'){
         const filter = {email: email}
         const updateDoc = {
           $set: {role: 'admin'},
         };
       const result = await userCollection.updateOne(filter, updateDoc);
       res.send(result);
-      }
-      else{
-        res.status(403).send({message: 'forbidden'});
-      }
     })
 
     // api for available slots
@@ -159,10 +166,25 @@ async function run(){
       return res.send({success: true, result});
     })
 
+    // api for manage all doctor
+    app.get('/doctor', verifyJWT, verifyAdmin,  async(req,res)=> {
+      const doctors = await doctorCollection.find().toArray();
+      res.send(doctors);
+    })
+
     // api for img upload
-    app.post('/doctor', async(req,res)=> {
+    app.post('/doctor', verifyJWT, verifyAdmin,   async(req,res)=> {
       const doctor = req.body;
       const result = await doctorCollection.insertOne(doctor);
+      res.send(result);
+    })
+    
+    // api for delete doctors 
+    
+    app.delete('/doctor/:email', verifyJWT, verifyAdmin,   async(req,res)=> {
+      const email = req.params.email;
+      const filter = {email: email};
+      const result = await doctorCollection.deleteOne(filter);
       res.send(result);
     })
 
