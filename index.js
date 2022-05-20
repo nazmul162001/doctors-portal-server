f4b6f0769727dccbcc291d79914c57d21bfc5f99
nazmul162001 <nazmulprime162001@gmail.com>
2022-05-20 12:09:25 +0600

    create api for make a payment

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const jwt = require('jsonwebtoken');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const verify = require('jsonwebtoken/verify');
 
 const app = express();
@@ -162,6 +162,15 @@ async function run() {
         return res.status(403).send({ message: 'forbidden access' });
       }
     });
+
+
+    app.get('/booking/:id', verifyJWT, async(req, res)=>{
+      const id = req.params.id;
+      const query = {_id: ObjectId(id)};
+      const booking = await bookingCollection.findOne(query);
+      res.send(booking);
+    })
+    
 
     // api for create new booking
     app.post('/booking', async (req, res) => {
