0e25c0a0b5a33fcc8acb6af76b7fe8642223180a
nazmul162001 <nazmulprime162001@gmail.com>
2022-05-21 16:51:19 +0600

    fixed bugs

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ const port = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors());
-// app.use(cors({origin: true}));
 app.use(express.json());
 
 // URI
@@ -189,7 +188,7 @@ async function run() {
       };
       const result = await paymentCollection.insertOne(payment);
       const updatedBooking = await bookingCollection.updateOne(filter, updatedDoc);
-      res.send(updatedDoc);
+      res.send(updatedBooking);
     })
 
 
