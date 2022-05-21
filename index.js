c57568752cd9a3c14b305c3f5aa648ef561de95b
nazmul162001 <nazmulprime162001@gmail.com>
2022-05-21 15:38:02 +0600

    fixed cors

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // Middleware
-// app.use(cors());
-app.use(cors({origin: true}));
+app.use(cors());
+// app.use(cors({origin: true}));
 app.use(express.json());
 
 // URI
